52d51e4cbb10bcb1cf0509da7c0e4b6bdcf8e172
Zoey Chesnut <zoeychesnut@Zoeys-MacBook-Air-2.local>
2026-03-18 13:35:08 -0400

    zoey-hotcollections-done

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,43 +1,121 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+import OwlCarousel from "react-owl-carousel";
+import { getHotCollections } from "../../API/nftAPI";
+import AOS from "aos";
+
+
 
 const HotCollections = () => {
+  const [collections, setCollections] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+
+
+
+    const loadData = async () => {
+      try {
+        const data = await getHotCollections();
+
+
+        setCollections(data || []);
+        AOS.refresh();
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadData();
+  }, []);
+
+  const options = {
+    loop: true,
+    margin: 30,
+    nav: true,
+    dots: false,
+    responsive: {
+  0: {
+    items: 1
+  },
+  576: {
+    items: 1
+  },
+  768: {
+    items: 2
+  },
+  992: {
+    items: 3
+  },
+  1200: {
+    items: 4
+  }
+}
+  };
+
+  if (loading) return <div style={{ padding: 80 }}>Loading...</div>;
+
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
-        <div className="row">
-          <div className="col-lg-12">
-            <div className="text-center">
-              <h2>Hot Collections</h2>
-              <div className="small-border bg-color-2"></div>
-            </div>
-          </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+        <div className="text-center">
+          <h2>Hot Collections</h2>
+          <div className="small-border bg-color-2"></div>
+        </div>
+
+        <OwlCarousel className="owl-theme" {...options}>
+          {collections.map((item) => (
+            <div className="item" 
+            key={item.id}
+            data-aos="zoom-in"
+            >
+
               <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
-                  </Link>
-                </div>
+
+                <div
+  className="nft_wrap"
+  style={{
+    backgroundImage: `url(${item.nftImage})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center"
+  }}
+>
+  <Link
+    to={`/item/${item.nftId}`}
+    style={{
+      display: "block",
+      width: "100%",
+      height: "100%",
+      cursor: "pointer"
+    }}
+  >
+    <span className="nft_coll_hover" />
+  </Link>
+</div>
+
                 <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
+  <Link to={`/author/${item.authorId}`}>
+    <img
+      className="pp-coll"
+      src={item.authorImage}
+      alt={item.authorName}
+    />
+    <i className="fa fa-check"></i>
+  </Link>
+</div>
+
                 <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <span>ERC-192</span>
-                </div>
+  <h4>{item.title}</h4>
+  <span>ERC-{item.code}</span>
+</div>
+
               </div>
             </div>
           ))}
-        </div>
+        </OwlCarousel>
       </div>
     </section>
   );
